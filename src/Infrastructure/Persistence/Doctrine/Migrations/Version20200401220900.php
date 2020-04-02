3adfedac10824882e7e73007811b20b879e00381
aurimas <auris.narkus@gmail.com>
2020-04-02 03:19:59 +0300

    Generated new migration

--- src/Infrastructure/Persistence/Doctrine/Migrations/Version20200401220900.php
+++ src/Infrastructure/Persistence/Doctrine/Migrations/Version20200401220900.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200330213008 extends AbstractMigration
+final class Version20200401220900 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,7 +22,7 @@ final class Version20200330213008 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE transactions (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, details VARCHAR(512) DEFAULT NULL, confirmation_code INT NOT NULL, status ENUM(\'created\', \'confirmed\', \'completed\', \'failed\') COMMENT \'(DC2Type:TransactionStatus)\' DEFAULT \'created\' NOT NULL, created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, recipient_account VARCHAR(255) NOT NULL, recipient_name VARCHAR(255) NOT NULL, amount VARCHAR(255) NOT NULL, currency CHAR(3) NOT NULL, fee_amount VARCHAR(255) NOT NULL, fee_currency CHAR(3) NOT NULL, INDEX user_id_index (user_id), INDEX user_id_currency_index (user_id, currency), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE transactions (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, amount VARCHAR(255) NOT NULL, fee VARCHAR(255) NOT NULL, total VARCHAR(255) NOT NULL, details VARCHAR(512) DEFAULT NULL, confirmation_code INT NOT NULL, status ENUM(\'created\', \'confirmed\', \'completed\', \'failed\') COMMENT \'(DC2Type:TransactionStatus)\' DEFAULT \'created\' NOT NULL, created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, recipient_account VARCHAR(255) NOT NULL, recipient_name VARCHAR(255) NOT NULL, currency CHAR(3) NOT NULL, INDEX user_id_index (user_id), INDEX user_id_currency_index (user_id, currency), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
     }
 
     public function down(Schema $schema) : void
